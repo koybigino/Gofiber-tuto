080a1295d913d8168cac5e92fd2fbd395ee4b76b
koybigino <koybigino@gmail.com>
2022-09-19 05:40:36 +0100

    Template & database

--- main.go
+++ main.go
@@ -3,32 +3,64 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Post struct {
-	Id        int    `json:"id"`
+	Id        int    `json:"id" gorm:"primaryKey"`
 	Title     string `json:"title"`
 	Content   string `json:"content"`
 	Published bool   `json:"published"`
+	gorm.Model
 }
 
 func main() {
-	app := fiber.New()
 
-	var posts []Post
+	dsn := "host=localhost user=postgres password=Bielem@*01 dbname=fiber_db port=5432 sslmode=disable TimeZone=UTC"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Database connection Error")
+	}
+
+	fmt.Println("Connection succed !")
+
+	if err := db.AutoMigrate(&Post{}); err != nil {
+		panic("Error ro create the table")
+	}
+
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Wolrd !")
+		var posts []Post
+		var count int64
+
+		db.Find(&posts).Count(&count)
+
+		return c.Render("index", fiber.Map{
+			"hello": "Hello World !",
+			"posts": posts,
+			"count": count,
+		})
 	})
 
 	// Get all posts
 	app.Get("/posts", func(c *fiber.Ctx) error {
+		var posts []Post
+		var count int64
+		db.Find(&posts).Count(&count)
 		return c.JSON(fiber.Map{
-			"posts": posts,
+			"posts":         posts,
+			"numberOfPosts": count,
 		})
 	})
 
@@ -44,16 +76,16 @@ func main() {
 			})
 		}
 
-		for _, post := range posts {
-			if post.Id == id {
-				return c.JSON(fiber.Map{
-					"posts": post,
-				})
-			}
-		}
+		post := new(Post)
 
+		if err := db.First(post, id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error":   err.Error(),
+				"message": fmt.Sprintf("Any Post correspond to this id = %d", id),
+			})
+		}
 		return c.JSON(fiber.Map{
-			"massage": fmt.Sprintf("Any Post correspond to this id = %d", id),
+			"post": post,
 		})
 	})
 
@@ -67,18 +99,23 @@ func main() {
 			})
 		}
 
-		newPost.Id = rand.Intn(100000)
+		if err := db.Create(newPost).Error; err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error":   err.Error(),
+				"message": "Error to createthe element !",
+			})
+		}
 
-		posts = append(posts, *newPost)
-
-		return c.Status(fiber.StatusCreated).JSON(fiber.Map{
-			"posts": newPost,
+		return c.JSON(fiber.Map{
+			"post": newPost,
 		})
+
 	})
 
 	// Upadate a post
 	app.Put("/posts/:id", func(c *fiber.Ctx) error {
 		strid := c.Params("id")
+		updatePost := new(Post)
 
 		id, err := strconv.Atoi(strid)
 		if err != nil {
@@ -87,29 +124,27 @@ func main() {
 			})
 		}
 
-		updatePost := new(Post)
-
-		for i, post := range posts {
-			if post.Id == id {
-				posts = append(posts[:i], posts[i+1:]...)
-
-				if err := c.BodyParser(updatePost); err != nil {
-					return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-						"error": err.Error(),
-					})
-				}
-
-				updatePost.Id = id
-				posts = append(posts, *updatePost)
-				return c.JSON(fiber.Map{
-					"posts": updatePost,
-				})
-
-			}
+		if err := c.BodyParser(updatePost); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": err.Error(),
+			})
 		}
 
+		post := new(Post)
+
+		if err := db.First(post, id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error":   err.Error(),
+				"message": fmt.Sprintf("Any Post correspond to this id = %d", id),
+			})
+		}
+
+		updatePost.Id = id
+
+		db.Save(updatePost)
+
 		return c.JSON(fiber.Map{
-			"massage": fmt.Sprintf("Any Post correspond to this id = %d", id),
+			"post": updatePost,
 		})
 
 	})
@@ -125,18 +160,18 @@ func main() {
 			})
 		}
 
-		for i, post := range posts {
-			if post.Id == id {
-				posts = append(posts[:i], posts[i+1:]...)
+		post := new(Post)
 
-				return c.SendStatus(fiber.StatusNoContent)
-
-			}
+		if err := db.First(post, id).Error; err != nil {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error":   err.Error(),
+				"message": fmt.Sprintf("Any Post correspond to this id = %d", id),
+			})
 		}
 
-		return c.JSON(fiber.Map{
-			"massage": fmt.Sprintf("Any Post correspond to this id = %d", id),
-		})
+		db.Delete(post)
+
+		return c.SendStatus(fiber.StatusNoContent)
 	})
 
 	// app.Static("/", "./public/index.html")
